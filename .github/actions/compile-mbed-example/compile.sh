e27fd8ed5cc52dd07a51f68132e32af979d67763
Matthew McGowan <mat.mcgowan+git@gmail.com>
2022-07-06 05:32:55 -0700

    more fixes

--- .github/actions/compile-mbed-example/compile.sh
+++ .github/actions/compile-mbed-example/compile.sh
@@ -3,6 +3,8 @@ EXAMPLE="$1"
 TARGET="$2"
 TOOLCHAIN="$3"
 
+set -e
+
 if [[ "$EXAMPLE" == "help" || -z "$EXAMPLE" ]]; then
 echo "Usage: compile.sh <example> <target> <toolchain>"
 exit 1
@@ -14,8 +16,10 @@ cp -r Notecard /mbed-ci/
 cp -r src/Notecard/* /mbed-ci/Notecard
 cp -r $EXAMPLE/* /mbed-ci
 
+python3 -m pip install -I intelhex==2.3.0
+
 pushd /mbed-ci
-mbed compile --target=$TARGET --toolchain=$TOOLCHAIN 
+mbed compile --target=$TARGET --toolchain=$TOOLCHAIN
 result=$?
 popd
 exit $result
